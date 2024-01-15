349df97c3e10cfe7bd979d2b5c6fad9f531c9fa0
aithihya-k <76926508+aithihya-k@users.noreply.github.com>
2024-01-15 12:04:59 -0500

    initial code for stir bar motor
    speed is set to 80% and PWM pins are 5 and 6

--- Car_Code/cheme_car_competition_codebase/cheme_car_competition_codebase.ino
+++ Car_Code/cheme_car_competition_codebase/cheme_car_competition_codebase.ino
@@ -5,8 +5,18 @@
 // Stir Bar Motor - Needs to be initialized, just constantly turn at 80% speed for now, we'll tune speed later.
 // Servo Motor - Needs to be initialized, turn 180 deg clockwise once at start to dump reactants, wait 1 sec to finish dumping, turn back 180 deg counter-clockwise to return to upright position, set speed to 100% for now, tune later.
 
-void setup() {
+// Define the PWM pins for the stir bar motor
+const int motorPin1 = 5;
+const int motorPin2 = 6;
 
+void setup() {
+  // Initialize the motor pins as outputs
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+
+  // Set the initial speed to 80%
+  analogWrite(motorPin1, 204); // 80% of 255
+  analogWrite(motorPin2, 204);
 }
 
 void loop() {
