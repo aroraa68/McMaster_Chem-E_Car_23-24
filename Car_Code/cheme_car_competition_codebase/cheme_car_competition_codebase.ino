46c8d7883701cd2dcc49d167dfb127b0c2511b46
aithihya-k <76926508+aithihya-k@users.noreply.github.com>
2024-01-15 12:25:44 -0500

    changed variable name from "motorPin" to "stirPin"

--- Car_Code/cheme_car_competition_codebase/cheme_car_competition_codebase.ino
+++ Car_Code/cheme_car_competition_codebase/cheme_car_competition_codebase.ino
@@ -11,12 +11,12 @@ const int stirPin2 = 6;
 
 void setup() {
   // Initialize the motor pins as outputs
-  pinMode(motorPin1, OUTPUT);
-  pinMode(motorPin2, OUTPUT);
+  pinMode(stirPin1, OUTPUT);
+  pinMode(stirPin2, OUTPUT);
 
   // Set the initial speed to 80%
-  analogWrite(motorPin1, 204); // 80% of 255
-  analogWrite(motorPin2, 204);
+  analogWrite(stirPin1, 204); // 80% of 255
+  analogWrite(stirPin2, 204);
 }
 
 void loop() {
