a222f72de49a3293a9bc31e45d7b20427ab08429
aithihya-k <76926508+aithihya-k@users.noreply.github.com>
2024-01-15 12:07:56 -0500

    initial code for stir bar motor
    same as with datalogging, pins 5 and 6 are set for PWM with constant speed set to 80%

--- Car_Code/cheme_car_datalogging_codebase/cheme_car_datalogging_codebase.ino
+++ Car_Code/cheme_car_datalogging_codebase/cheme_car_datalogging_codebase.ino
@@ -6,8 +6,18 @@
 // Servo Motor - Needs to be initialized, turn 180 deg clockwise once at start to dump reactants, wait 1 sec to finish dumping, turn back 180 deg counter-clockwise to return to upright position, set speed to 100% for now, tune later.
 // MicroSD Card - Store all variable data to MicroSD Card on microcontroller, allow for each run to be saved under a different filename (potentially in CSV format) so we can keep track of everything.
 
-void setup() {
+// Define the PWM pins for the stir bar motor
+const int motorPin1 = 5;
+const int motorPin2 = 6;
 
+void setup() {
+  // Initialize the motor pins as outputs
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+
+  // Set the initial speed to 80%
+  analogWrite(motorPin1, 204); // 80% of 255
+  analogWrite(motorPin2, 204);
 }
 
 void loop() {
