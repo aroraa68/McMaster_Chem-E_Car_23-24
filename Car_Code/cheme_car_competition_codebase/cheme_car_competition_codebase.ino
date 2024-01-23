e19bc4afb23e6faf05201591b5daf08280a7e4ba
AmanRV <amanvarghese7@gmail.com>
2024-01-22 19:57:54 -0500

    drive forward and stopping code

--- Car_Code/cheme_car_competition_codebase/cheme_car_competition_codebase.ino
+++ Car_Code/cheme_car_competition_codebase/cheme_car_competition_codebase.ino
@@ -5,10 +5,52 @@
 // Stir Bar Motor - Needs to be initialized, just constantly turn at 80% speed for now, we'll tune speed later.
 // Servo Motor - Needs to be initialized, turn 180 deg clockwise once at start to dump reactants, wait 1 sec to finish dumping, turn back 180 deg counter-clockwise to return to upright position, set speed to 100% for now, tune later.
 
+#define left_pwm1 9
+#define left_pwm2 10
+#define right_pwm1 11
+#define right_pwm2 12
+
 void setup() {
+
+  //Setting to output mode
+  pinMode(left_pwm1, OUTPUT);
+  pinMode(left_pwm2, OUTPUT);
+  pinMode(right_pwm1, OUTPUT);
+  pinMode(right_pwm2, OUTPUT);
+
+  //start motors completely stopped
+  analogWrite(left_pwm1, 0);
+  digitalWrite(left_pwm2, LOW);
+  analogWrite(right_pwm1, 0);
+  digitalWrite(right_pwm2, LOW);
 
 }
 
 void loop() {
 
+    drive_forward(204); //80% speed is 204
+    //stop_driving();  //uncomment to stop
+
 }
+
+void drive_forward(int speed){
+  //left wheel
+  analogWrite(left_pwm1, speed);
+  digitalWrite(left_pwm2, LOW);
+
+  //right wheel
+  analogWrite(right_pwm1, speed);
+  digitalWrite(right_pwm2, LOW);
+
+}
+
+void stop_driving(){
+  //left wheel
+  analogWrite(left_pwm1, 0);
+  digitalWrite(left_pwm2, LOW);
+
+  //right wheel
+  analogWrite(right_pwm1, 0);
+  digitalWrite(right_pwm2, LOW);
+}
+
